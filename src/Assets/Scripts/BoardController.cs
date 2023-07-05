f065b9a1a44def74641fdece6d6a7b997d933c9d
suijumoku <annryuuki@gmail.com>
2023-07-05 23:38:42 +0900

    コミット11

--- src/Assets/Scripts/BoardController.cs
+++ src/Assets/Scripts/BoardController.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+struct FallData
+{
+    public readonly int X { get; }
+    public readonly int Y { get; }
+    public readonly int Dest { get; }// 落ちる先
+    public FallData(int x, int y, int dest)
+    {
+        X=x;
+        Y=y;
+        Dest=dest;
+    }
+
+}
 public class BoardController : MonoBehaviour
 {
+    public const int FALL_FRAME_PER_CELL = 5;// 単位セル当たりの落下フレーム
     public const int BOARD_WIDTH = 6;
     public const int BOARD_HEIGHT = 14;
 
@@ -11,6 +26,10 @@ public class BoardController : MonoBehaviour
 
     int[,] _board=new int[BOARD_HEIGHT, BOARD_WIDTH];
     GameObject[,] _Puyos=new GameObject[BOARD_HEIGHT, BOARD_WIDTH];
+
+    //落ちる際の一次的関数
+    List<FallData> _falls = new();
+    int _fallFrames = 0;
 
     private void ClearAll()
     {
@@ -28,14 +47,6 @@ public class BoardController : MonoBehaviour
     public void Start()
     {
         ClearAll();
-
-        //for(int y=0;y<BOARD_HEIGHT;y++)
-        //{
-        //    for(int x=0; x<BOARD_WIDTH;x++)
-        //    {
-        //        Settle(new Vector2Int(x, y), Random.Range(1, 7));
-        //    }
-        //}
     }
 
 
@@ -63,5 +74,68 @@ public class BoardController : MonoBehaviour
         _Puyos[pos.y, pos.x].GetComponent<PuyoController>().SetPuyoType((PuyoType)val);
 
         return true;
+    }
+
+    public bool CheckFall()
+    {
+        _falls.Clear();
+        _fallFrames = 0;
+
+        // 落ちる先の高さの記録用
+        int[] dsts = new int[BOARD_WIDTH];
+        for(int x = 0; x < BOARD_WIDTH; x++) dsts[x] = 0;
+
+        int max_check_line = BOARD_HEIGHT - 1;// 実はぷよぷよ通では最上段は落ちてこない
+        for(int y=0;y<max_check_line;y++)// 下から上に検索
+        {
+            for(int x=0;x<BOARD_WIDTH;x++)
+            {
+                if (_board[y, x] == 0) continue;
+
+                int dst = dsts[x];
+                dsts[x] = y+1;// 上のぷよが落ちてくるなら自分の上
+
+                if (y == 0) continue;// 一番下なら落ちない
+
+                if (_board[y - 1, x] != 0) continue;// 下があれば除外
+
+                _falls.Add(new FallData(x, y, dst));
+
+                // データを変更しておく
+                _board[dst, x] = _board[y,x];
+                _board[y, x] = 0;
+                _Puyos[dst, x] = _Puyos[y, x];
+                _Puyos[y, x] = null;
+
+                dsts[x] = dst + 1;// 次の物は落ちたさらに上に乗る
+            }
+        }
+
+        return _falls.Count != 0;
+    }
+
+    public bool Fall()
+    {
+        _fallFrames++;
+
+        float dy = _fallFrames / (float)FALL_FRAME_PER_CELL;
+        int di = (int)dy;
+
+        for(int i=_falls.Count-1;0<=i;i--)// ループ中で削除しても安全なように後ろから検索
+        {
+            FallData f = _falls[i];
+
+            Vector3 pos = _Puyos[f.Dest, f.X].transform.localPosition;
+            pos.y = f.Y - dy;
+
+            if(f.Y<=f.Dest+di)
+            {
+                pos.y = f.Dest;
+                _falls.RemoveAt(i);
+            }
+            _Puyos[f.Dest,f.X].transform.localPosition = pos;// 表示位置の更新
+
+        }
+        return _falls.Count != 0;
     }
 }
--- src/Assets/Scripts/PlayDirector.cs
+++ src/Assets/Scripts/PlayDirector.cs
@@ -9,6 +9,7 @@ interface IState
     {
         Control=0,
         GameOver=1,
+        Falling=2,
 
         MAX,
 
@@ -18,28 +19,33 @@ interface IState
     E_State Initialize(PlayDirector parent);
     E_State Update(PlayDirector parent);
 }
+[RequireComponent(typeof(BoardController))]
 public class PlayDirector : MonoBehaviour
 {
     [SerializeField] GameObject player = default!;
     PlayerController _playerController=null;
     Logicallnput _logicalInput = new();
+    BoardController _boardController = default!;
 
     NextQueue _nextQueue = new();
     [SerializeField] PuyoPair[] nextPuyoPairs = { default!, default! }; // 次のnextのゲームオブジェクトの制御
 
     // 状態管理
-    IState.E_State _current_state=IState.E_State.Control;
+    IState.E_State _current_state=IState.E_State.Falling;
     static readonly IState[] states = new IState[(int)IState.E_State.MAX]
     {
         new ControlState(),
         new GameOverState(),
+        new FallingState(),
     };
     // Start is called before the first frame update
     void Start()
     {
         _playerController = player.GetComponent<PlayerController>();
+        _boardController=GetComponent<BoardController>();
         _logicalInput.Clear();
         _playerController.SetLogicalInput(_logicalInput);
+
 
         _nextQueue.Initialize();
         // 状態の初期化
@@ -107,7 +113,19 @@ public class PlayDirector : MonoBehaviour
         }
         public IState.E_State Update(PlayDirector parent)
         {
-            return parent.player.activeSelf ? IState.E_State.Unchanged : IState.E_State.Control;
+            return parent.player.activeSelf ? IState.E_State.Unchanged : IState.E_State.Falling;
+        }
+    }
+
+    class FallingState:IState
+    {
+        public IState.E_State Initialize(PlayDirector parent)
+        {
+            return parent._boardController.CheckFall() ? IState.E_State.Unchanged : IState.E_State.Control;
+        }
+        public IState.E_State Update(PlayDirector parent)
+        {
+            return parent._boardController.Fall() ? IState.E_State.Unchanged : IState.E_State.Control;
         }
     }
 
