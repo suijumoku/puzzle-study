83bc9a67bc23325a97e79bf9c1800bb84ce11c52
suijumoku <annryuuki@gmail.com>
2023-07-22 00:43:15 +0900

    コミット12

--- src/Assets/Scripts/BoardController.cs
+++ src/Assets/Scripts/BoardController.cs
@@ -10,9 +10,9 @@ struct FallData
     public readonly int Dest { get; }// 落ちる先
     public FallData(int x, int y, int dest)
     {
-        X=x;
-        Y=y;
-        Dest=dest;
+        X = x;
+        Y = y;
+        Dest = dest;
     }
 
 }
@@ -22,25 +22,29 @@ public class BoardController : MonoBehaviour
     public const int BOARD_WIDTH = 6;
     public const int BOARD_HEIGHT = 14;
 
-    [SerializeField]GameObject prefabPuyo = default!;
+    [SerializeField] GameObject prefabPuyo = default!;
 
-    int[,] _board=new int[BOARD_HEIGHT, BOARD_WIDTH];
-    GameObject[,] _Puyos=new GameObject[BOARD_HEIGHT, BOARD_WIDTH];
+    int[,] _board = new int[BOARD_HEIGHT, BOARD_WIDTH];
+    GameObject[,] _Puyos = new GameObject[BOARD_HEIGHT, BOARD_WIDTH];
 
-    //落ちる際の一次的関数
+    // 落ちる際の一次的変数
     List<FallData> _falls = new();
     int _fallFrames = 0;
+
+    // 削除する際の一次的変数
+    List<Vector2Int> _erases = new();
+    int _erasesFrames = 0;
 
     private void ClearAll()
     {
         for (int y = 0; y < BOARD_HEIGHT; y++)
         {
-            for(int x=0;x<BOARD_WIDTH;x++)
+            for (int x = 0; x < BOARD_WIDTH; x++)
             {
-                _board[y,x] = 0;
+                _board[y, x] = 0;
 
-                if (_Puyos[y,x]!= null) Destroy(_Puyos[y,x]);
-                _Puyos[y,x]=null;
+                if (_Puyos[y, x] != null) Destroy(_Puyos[y, x]);
+                _Puyos[y, x] = null;
             }
         }
     }
@@ -62,9 +66,9 @@ public class BoardController : MonoBehaviour
 
         return 0 == _board[pos.y, pos.x];
     }
-    public bool Settle(Vector2Int pos,int val)
+    public bool Settle(Vector2Int pos, int val)
     {
-        if(!CanSettle(pos))return false;
+        if (!CanSettle(pos)) return false;
 
         _board[pos.y, pos.x] = val;
 
@@ -83,17 +87,17 @@ public class BoardController : MonoBehaviour
 
         // 落ちる先の高さの記録用
         int[] dsts = new int[BOARD_WIDTH];
-        for(int x = 0; x < BOARD_WIDTH; x++) dsts[x] = 0;
+        for (int x = 0; x < BOARD_WIDTH; x++) dsts[x] = 0;
 
         int max_check_line = BOARD_HEIGHT - 1;// 実はぷよぷよ通では最上段は落ちてこない
-        for(int y=0;y<max_check_line;y++)// 下から上に検索
+        for (int y = 0; y < max_check_line; y++)// 下から上に検索
         {
-            for(int x=0;x<BOARD_WIDTH;x++)
+            for (int x = 0; x < BOARD_WIDTH; x++)
             {
                 if (_board[y, x] == 0) continue;
 
                 int dst = dsts[x];
-                dsts[x] = y+1;// 上のぷよが落ちてくるなら自分の上
+                dsts[x] = y + 1;// 上のぷよが落ちてくるなら自分の上
 
                 if (y == 0) continue;// 一番下なら落ちない
 
@@ -102,7 +106,7 @@ public class BoardController : MonoBehaviour
                 _falls.Add(new FallData(x, y, dst));
 
                 // データを変更しておく
-                _board[dst, x] = _board[y,x];
+                _board[dst, x] = _board[y, x];
                 _board[y, x] = 0;
                 _Puyos[dst, x] = _Puyos[y, x];
                 _Puyos[y, x] = null;
@@ -121,21 +125,105 @@ public class BoardController : MonoBehaviour
         float dy = _fallFrames / (float)FALL_FRAME_PER_CELL;
         int di = (int)dy;
 
-        for(int i=_falls.Count-1;0<=i;i--)// ループ中で削除しても安全なように後ろから検索
+        for (int i = _falls.Count - 1; 0 <= i; i--)// ループ中で削除しても安全なように後ろから検索
         {
             FallData f = _falls[i];
 
             Vector3 pos = _Puyos[f.Dest, f.X].transform.localPosition;
             pos.y = f.Y - dy;
 
-            if(f.Y<=f.Dest+di)
+            if (f.Y <= f.Dest + di)
             {
                 pos.y = f.Dest;
                 _falls.RemoveAt(i);
             }
-            _Puyos[f.Dest,f.X].transform.localPosition = pos;// 表示位置の更新
+            _Puyos[f.Dest, f.X].transform.localPosition = pos;// 表示位置の更新
 
         }
         return _falls.Count != 0;
+    }
+
+    static readonly Vector2Int[] search_tbl = new Vector2Int[] { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
+
+    // 消えるぷよを検索する（同じ色を上下左右に見つけていく。見つけたらフラグを立てて再計算しない）
+    public bool CheckErase()
+    {
+        _erasesFrames = 0;
+        _erases.Clear();
+
+        uint[] isChecked = new uint[BOARD_HEIGHT];// メモリを多く使うのは無駄なのでビット処理
+
+        List<Vector2Int> add_list = new();
+        for (int y = 0; y < BOARD_HEIGHT; y++)
+        {
+            for (int x = 0; x < BOARD_WIDTH; x++)
+            {
+                if ((isChecked[y] & (1u << x)) != 0) continue;// 調査済み
+
+                isChecked[y] |= (1u << x);
+
+                int type = _board[y, x];
+                if (type == 0) continue;// 空間だった
+
+                System.Action<Vector2Int> get_connection = null;// 再帰で使う場合に必要
+                get_connection = (pos) =>
+                {
+                    add_list.Add(pos);// 削除対象とする
+
+                    foreach (Vector2Int d in search_tbl)
+                    {
+                        Vector2Int target = pos + d;
+                        if (target.x < 0 || BOARD_WIDTH <= target.x ||
+                            target.y < 0 || BOARD_HEIGHT <= target.y) continue;// 範囲外
+                        if (_board[target.y, target.x] != type) continue;// 色違い
+                        if ((isChecked[target.y] & (1u << target.x)) != 0) continue;// 検索済み
+
+                        isChecked[target.y] |= (1u << target.x);
+                        get_connection(target);
+                    }
+
+                };
+
+                add_list.Clear();
+                get_connection(new Vector2Int(x, y));
+
+                if (4 <= add_list.Count)
+                {
+                    _erases.AddRange(add_list);
+                }
+            }
+        }
+        return _erases.Count != 0;
+    }
+
+    public bool Erase()
+    {
+        _erasesFrames++;
+
+        // 1から不得手ちょっとしたら最大に大きくなった後小さくなって消える
+        float t = _erasesFrames * Time.deltaTime;
+        t = 1.0f - 10.0f * ((t - 0.1f) * (t - 0.1f) - 0.1f * 0.1f);
+
+        // 大きさが負ならおしまい
+        if (t <= 0.0f)
+        {
+            // データとゲームオブジェクトをここで消す
+            foreach (Vector2Int d in _erases)
+            {
+                Destroy(_Puyos[d.y, d.x]);
+                _Puyos[d.y, d.x] = null;
+                _board[d.y, d.x] = 0;
+            }
+
+            return false;
+        }
+
+        // モデルの大きさを変える
+        foreach (Vector2Int d in _erases)
+        {
+            _Puyos[d.y, d.x].transform.localScale = Vector3.one * t;
+        }
+
+        return true;
     }
 }
--- src/Assets/Scripts/PlayDirector.cs
+++ src/Assets/Scripts/PlayDirector.cs
@@ -10,6 +10,7 @@ interface IState
         Control=0,
         GameOver=1,
         Falling=2,
+        Erasing=3,
 
         MAX,
 
@@ -37,6 +38,7 @@ public class PlayDirector : MonoBehaviour
         new ControlState(),
         new GameOverState(),
         new FallingState(),
+        new ErasingState(),
     };
     // Start is called before the first frame update
     void Start()
@@ -121,11 +123,23 @@ public class PlayDirector : MonoBehaviour
     {
         public IState.E_State Initialize(PlayDirector parent)
         {
-            return parent._boardController.CheckFall() ? IState.E_State.Unchanged : IState.E_State.Control;
+            return parent._boardController.CheckFall() ? IState.E_State.Unchanged : IState.E_State.Erasing;
         }
         public IState.E_State Update(PlayDirector parent)
         {
-            return parent._boardController.Fall() ? IState.E_State.Unchanged : IState.E_State.Control;
+            return parent._boardController.Fall() ? IState.E_State.Unchanged : IState.E_State.Erasing;
+        }
+    }
+
+    class ErasingState : IState
+    {
+        public IState.E_State Initialize(PlayDirector parent)
+        {
+            return parent._boardController.CheckErase() ? IState.E_State.Unchanged : IState.E_State.Control;
+        }
+        public IState.E_State Update(PlayDirector parent)
+        {
+            return parent._boardController.Erase() ? IState.E_State.Unchanged : IState.E_State.Falling;
         }
     }
 
